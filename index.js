93ede37c21665bb48d2cb77c46b36b3b1c1c77d0
CasdeBruijnEHB <71756854+CasdeBruijnEHB@users.noreply.github.com>
2021-11-30 09:59:31 +0100

    SecondUpdate

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static("public"));
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    console.log("local root called");
+    res.redirect('/info.html')
+})
+app.get('/challenges', (req, res) => {
+
+    res.send('All the challenges')
+})
+app.post('/challenges', (req, res) => {
+
+})
+
+app.put('/challenges/:id', (req, res) => {
+
+})
+
+app.delete(' /challenges/:id', (req, res) => {
+
 })
 
 app.listen(port, () => {

bab593a8da9fb9d990007606bb4c54695c8eff8f
Rhys Devalckeneer <rhys.devalckeneer@student.ehb.be>
2021-12-04 13:35:34 +0100

    Updated index.js
    COMPLETE: Get data from /challenges

---
+++ index.js
@@ -0,0 +1,46 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+
+// create mongo client
+const client = new MongoClient(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const app = express();
+const port = process.env.PORT || 1337;
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+
+// basic route
+app.get('/', (req, res) => {
+  res.status(300).redirect('/info.html');
+})
+
+app.get('/challenges', async ( req, res) => {
+    try {
+        // connect to database
+        await client.connect();
+        const collection = client.db('Session7').collection('challenges')
+        const challenges = await collection.find({}).toArray();
+
+        // send back data with response
+        res.status(200).send(challenges);
+    } catch(error){
+        console.log(error)
+        // return error when something is wrong
+        res.status(500).send({
+            error: 'Something went wrong',
+            value: error
+        });
+    } finally {
+        await client.close();
+    }
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})

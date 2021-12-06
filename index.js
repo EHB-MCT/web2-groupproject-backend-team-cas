ebb279d5c9ca2ac2bfdec1d8188707e03bbe01c5
Rhys Devalckeneer <rhys.devalckeneer@student.ehb.be>
2021-12-06 11:08:26 +0100

    u

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 1337;
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'no-cors');
+    res.header('Access-Control-Allow-Origin', '*');
     next();
 });
 

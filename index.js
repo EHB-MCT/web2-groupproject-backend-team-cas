cce75ce773ac3039dc1c239fc898cb26ee68fc16
Rhys Devalckeneer <rhys.devalckeneer@student.ehb.be>
2021-12-10 22:32:05 +0100

    edited

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-const { MongoClient } = require('mongodb');
-require('dotenv').config()
+const { MongoClient, ObjectId } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
+const { resetWatchers } = require('nodemon/lib/monitor/watch');
 
 // create mongo client
 const client = new MongoClient(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -12,12 +14,7 @@ const port = process.env.PORT || 1337;
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-
-// CORS error - https://medium.com/@dtkatz/3-ways-to-fix-the-cors-error-and-how-access-control-allow-origin-works-d97d55946d9
- app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
- });
+app.use(cors());
 
 
 // basic route
@@ -47,9 +44,8 @@ app.get('/challenges', async ( req, res) => {
     }
 });
 
-
 // get one challenge
-app.get('/challenge', async (req,res) => {
+app.get('/challenge/:id', async (req,res) => {
     
     //id is located in the query: req.query.id
     try{
@@ -83,112 +79,147 @@ app.get('/challenge', async (req,res) => {
     }
 });
 
-app.post('/saveChallenge', async (req, res) =>{
+app.post('/challenges', async (req, res) => {
 
-    console.log(req.body);
-
-    if(!req.body.name || !req.body.points || !req.body.course || !req.body.session || !req.body.id) {
-        req.status(400).send('Bad request: missing name, points, course, session or id');
-        return;
-    }
-
-    try {
+    try{
+         //connect to the db
         await client.connect();
-        const collection = client.db('Session7').collection('challenges')
-        
-        const challenge = await collection.findOne( {id: req.body.id} )
-        if(challenge) {
-            res.status(400).send('Bad request: Challenge already exists with that id ' + req.body.id); 
+
+         //retrieve the challenges collection data
+        const collection = client.db('Session7').collection('challenges');
+
+         // Validation for double challenges
+        const challenge = await collection.findOne({_id: ObjectId(req.params.id)});
+        if(challenge){
+            res.status(400).send(`Bad request: Challenge already exists with name ${req.body.name} for course ${req.body.course}` );
             return;
-        }
-
+        } 
+         // Create the new Challenge object
         let newChallenge = {
-            id: req.body.id,
             name: req.body.name,
+            course: req.body.course,
             points: req.body.points,
-            course: req.body.points,
-            session: req.body.session,
         }
-
-        await collection.insertOne(newChallenge);
-
-        res.status(201).send(newChallenge);
-        return;
+        // Add the optional session field
+        if(req.body.session){
+            newChallenge.session = req.body.session;
+        }
         
+         // Insert into the database
+        let insertResult = await collection.insertOne(newChallenge);
+
+         //Send back successmessage
+        res.status(201).json(newChallenge);
+        return;
     }catch(error){
         console.log(error);
         res.status(500).send({
             error: 'Something went wrong',
             value: error
-        })
-
-    }finally{
+        });
+    }finally {
         await client.close();
     }
-})
+});
 
-app.put('/challenge', (req, res) => {
-    if (!req.body.name || !req.body.course || !req.body.points) {
-        res.status(400).send('Request failed — Missing name, course or points attributed');
+app.put('/challenges/:id', async (req,res) => {
+    //Check for body data
+    if(!req.body.name || !req.body.points || !req.body.course || !req.body.session){
+        res.status(400).send({
+            error: 'Bad Request',
+            value: 'Missing name, course or points property'
+        });
+        return;
+    }
+    // Check for id in url
+    if(!req.params.id){
+        res.status(400).send({
+            error: 'Bad Request',
+            value: 'Missing id in url'
+        });
         return;
     }
 
-    try {
+    try{
+         //connect to the db
+        await client.connect();
 
-        const collections = client.db('Session7').collection('challenges');
-        const query = { id: Number(req.query.id) };
+         //retrieve the challenges collection data
+        const collection = client.db('Session7').collection('challenges');
 
-        const challenge = await collections.findOne(query);
-
-        let updChallenge = {
+         // Validation for existing challenge
+        const challenge = await collection.findOne({_id: ObjectId(req.params.id)});
+        if(!challenge){
+            res.status(400).send({
+                error: 'Bad Request',
+                value: `Challenge does not exist with id ${req.params.id}`
+            });
+            return;
+        } 
+         // Create the new Challenge object
+        let newChallenge = {
             name: req.body.name,
             course: req.body.course,
-            points: req.body.points
+            points: req.body.points,
+            session: req.body.session
         }
-
+        // Add the optional session field
         if(req.body.session){
             newChallenge.session = req.body.session;
         }
+        
+         // Insert into the database
+        let updateResult = await collection.updateOne({_id: ObjectId(req.params.id)}, 
+        {$set: newChallenge});
 
-        await challenge.updateChallenge(updChallenge)
-
-
-
-    }catch(error) {
+         //Send back successmessage
+        res.status(201).json(updateResult);
+        return;
+    }catch(error){
         console.log(error);
         res.status(500).send({
-            error: 'Something went wrong', 
+            error: 'Something went wrong',
             value: error
         });
     }finally {
         await client.close();
     }
-
-
-})
-
-app.delete('/challenge', (req, res) => {
-
-    try {
-
-        const collections = client.db('Session7').collection('challenges');
-        const query = { id: Number(req.query.id) };
-
-        const challenge = await collections.findOne(query);
-
-
-
-    }catch(error) {
-        console.log(error);
-        res.status(500).send({
-            error: 'Something went wrong', 
-            value: error
-        });
-    }finally {
-        await client.close();
-    }
-    
 });
+
+
+//delete a challenge
+app.delete('/challenges/:id', async (req,res) => {
+    
+    if(!req.params.id){
+        res.status(400).send({
+            error: 'Bad Request',
+            value: 'No id available in url'
+        });
+        return;
+    }
+    try{
+         //connect to the db
+        await client.connect();
+
+         //retrieve the challenges collection data
+        const collection = client.db('Session7').collection('challenges');
+
+         // Validation for double challenges
+        let result = await collection.deleteOne({_id: ObjectId(req.params.id)});
+         //Send back successmessage
+        res.status(201).json(result);
+        return;
+    }catch(error){
+        console.log(error);
+        res.status(500).send({
+            error: 'Something went wrong',
+            value: error
+        });
+    }finally {
+        await client.close();
+    }
+});
+
 
 
 
